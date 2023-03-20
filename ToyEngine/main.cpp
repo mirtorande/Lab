968ed54961bb7e15b2b52b5ca5aaf5f84b12ac70
mirtorande <mirtorande@gmail.com>
2023-03-20 19:01:06 +0100

    + Unit test on sum

--- ToyEngine/main.cpp
+++ ToyEngine/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <assert.h>
 
 typedef float Scalar;
 
@@ -29,10 +30,24 @@ Scalar Vector::dot(const Vector &other) const {
     return x * other.x + y * other.y + z * other.z;
 }
 
+bool isEqual(Scalar s1, Scalar s2) {
+    return (s1-s2)*(s1-s2) < 1e-8;
+}
+
+bool isEqual(Vector v1, Vector v2) {
+    return isEqual(v1.x, v2.x) && isEqual(v1.y, v2.y) && isEqual(v1.z, v2.z);
+}
+
+// Tests
+void unitTestSumIsCommutative() {
+    Vector v(1, 2, 3), w(4, 5, 6);
+    assert(isEqual(v+w, w+v));
+}
+
 int main() {
     Vector v, w(2, 3, 5);
     v.x = 2;
     v = v + w;
-    Scalar k = v.dot(w);
+    unitTestSumIsCommutative();
     return 0;
 }
