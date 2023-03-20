0accb20c97997b06d8be3c97300fb512cf05200a
mirtorande <mirtorande@gmail.com>
2023-03-20 18:41:42 +0100

    Vector sum and dot product

--- ToyEngine/main.cpp
+++ ToyEngine/main.cpp
@@ -4,17 +4,30 @@ typedef float Scalar;
 
 class Vector {
 public:
-    //float coords [3];
     Scalar x, y, z;
 
-    Vector sum(Vector other) {
+    Vector();
+    Vector(Scalar a, Scalar b, Scalar c);
 
-    }
+    Vector sum(Vector other) const;
+    Scalar dot(Vector other) const;
 };
 
+Vector::Vector(): x(0), y(0), z(0) {}
+Vector::Vector(Scalar a, Scalar b, Scalar c): x(a), y(b), z(c) {}
+
+Vector Vector::sum(Vector other) const {
+    return Vector(x + other.x, y + other.y, z + other.z);
+}
+
+Scalar Vector::dot(Vector other) const {
+    return x * other.x + y * other.y + z * other.z;
+}
+
 int main() {
-    Vector v;
-    //v.coords[0] = 2;
+    Vector v, w(2, 3, 5);
     v.x = 2;
+    v = v.sum(w);
+    Scalar k = v.dot(w);
     return 0;
 }
