9144025c53875db29b24b75b20c2c088d273e0a0
mirtorande <mirtorande@gmail.com>
2023-03-20 18:48:15 +0100

    Addition with '+' operator

--- ToyEngine/main.cpp
+++ ToyEngine/main.cpp
@@ -9,25 +9,30 @@ public:
     Vector();
     Vector(Scalar a, Scalar b, Scalar c);
 
-    Vector sum(Vector other) const;
-    Scalar dot(Vector other) const;
+    Vector sum(const Vector &other) const;
+    Vector operator+(const Vector &other) const;
+    Scalar dot(const Vector &other) const;
 };
 
 Vector::Vector(): x(0), y(0), z(0) {}
 Vector::Vector(Scalar a, Scalar b, Scalar c): x(a), y(b), z(c) {}
 
-Vector Vector::sum(Vector other) const {
+Vector Vector::sum(const Vector &other) const {
     return Vector(x + other.x, y + other.y, z + other.z);
 }
 
-Scalar Vector::dot(Vector other) const {
+Vector Vector::operator+(const Vector &other) const {
+    return Vector(x + other.x, y + other.y, z + other.z);
+}
+
+Scalar Vector::dot(const Vector &other) const {
     return x * other.x + y * other.y + z * other.z;
 }
 
 int main() {
     Vector v, w(2, 3, 5);
     v.x = 2;
-    v = v.sum(w);
+    v = v + w;
     Scalar k = v.dot(w);
     return 0;
 }
