3d187c65bc7abf8ccd15616deaf65b5ea35a8883
mirtorande <mirtorande@gmail.com>
2023-03-20 18:16:01 +0100

    first commit

---
+++ ToyEngine/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+typedef float Scalar;
+
+class Vector {
+public:
+    //float coords [3];
+    Scalar x, y, z;
+
+    Vector sum(Vector other) {
+
+    }
+};
+
+int main() {
+    Vector v;
+    //v.coords[0] = 2;
+    v.x = 2;
+    return 0;
+}
